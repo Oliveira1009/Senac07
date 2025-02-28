976b4796abebd9e6e39b378b7e1cf8154317901a
TAMIRES OLIVEIRA <tamires.oliveira6@senacsp.edu.br>
2025-02-27 22:12:17 -0300

    terceira aula

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -2,6 +2,9 @@ namespace WinFormsApp1
 {
     public partial class form1 : Form
     {
+        List<string> listaUsuarios = new List<string>() { "lari.Matos", "kaique", "mickael" };
+        List<string> Listasenha = new List<string>() { "1717", "0707", "0303" };
+
         public form1()
         {
             InitializeComponent();
@@ -30,30 +33,39 @@ namespace WinFormsApp1
 
         private void Entrar_Click(object sender, EventArgs e)
         {
-            string usuario = BoxNome.Text;
+            string usuarioBuscado = BoxNome.Text;
             string senha = BoxAcesso.Text;
 
-            if (usuario == null || usuario == "" || string.IsNullOrWhiteSpace (usuario))
+            if (string.IsNullOrWhiteSpace(usuarioBuscado))
             {
-                labelResultado.Text = "usuario eh obrgatorio!!!";
+               labelResultado.Text = "usuario eh obrgatorio!!!";
                 labelResultado.ForeColor = Color.Red;
                 return;
             }
-            
-            if (usuario == null || senha == "")
+
+            if (senha == null || senha == "")
             {
                 labelResultado.Text = "Senha necessaria!!!";
                 labelResultado.ForeColor = Color.Red;
                 return;
             }
 
-            if (usuario == "tamiresoliveira" && senha == "0707")
+            int posicaoUsuarioEncontrado = -1;
+                for (int i = 0; i < listaUsuarios.Count; i++)
+                {
+                    if (usuarioBuscado == listaUsuarios[i])
+                    {
+                        posicaoUsuarioEncontrado = i;
+                    }
+                }
+
+            if (posicaoUsuarioEncontrado > -1 && senha == "1717")
             {
                 labelResultado.Text = "Autenticado com sucesso !";
                 labelResultado.ForeColor = Color.Green;
-
             }
-            else if (usuario == "tamiresoliveira" && senha == "")
+
+            else if (usuarioBuscado == "tamiresoliveira" && senha == "")
             {
                 labelResultado.Text = " Você deve colocar senha para acesso";
                 labelResultado.ForeColor = Color.Violet;
--- WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Debug/net8.0-windows/login.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("login")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+224874cae03fc7c80e3a7e1469fd11a10010e8ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("login")]
 [assembly: System.Reflection.AssemblyTitleAttribute("login")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
