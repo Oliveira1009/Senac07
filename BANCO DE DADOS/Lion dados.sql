6f83b3b4dd326cfc194ca7a528f4df54c2eee573
TAMIRES OLIVEIRA <tamires.oliveira6@senacsp.edu.br>
2025-03-25 22:14:57 -0300

    banco de dados

---
+++ BANCO DE DADOS/Lion dados.sql
@@ -0,0 +1,24 @@
+select
+*
+From
+usuario
+where
+id=1;
+select 
+*
+from
+usuario
+where
+nome = 'Lari Matos';
+select
+*
+from
+usuario
+where
+email like 'LariMatos@email.com';
+select
+*
+from
+usuario
+where
+senha like 'Lariee@';
---
+++ BANCO DE DADOS/Usuario Informações.sql
@@ -0,0 +1,16 @@
+create table filme (
+ id INT AUTO_INCREMENT PRIMARY KEY, 
+ titulo varchar (225)  not null,
+ genero varchar (100),
+ duracao int,
+ atores varchar (255),  
+ idioma varchar (50),
+ pais_de_origem varchar (100),
+ personagem varchar (100)
+ );
+ select
+ *
+ from
+ filme,
+ 
+ insert into
