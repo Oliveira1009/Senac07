819b0f774e2b28bc487b94450162728bad642053
TAMIRES OLIVEIRA <tamires.oliveira6@senacsp.edu.br>
2025-04-01 22:08:03 -0300

    BANCO DE DADOS 4

--- BANCO DE DADOS/tamiresoliveira.sql
+++ BANCO DE DADOS/tamiresoliveira.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF  NOT EXISTS EMPREGADO(
 id INT PRIMARY KEY AUTO_INCREMENT ,
 nome VARCHAR (100) NOT NULL,
-cargo VARCHAR (50) NOT NULL,
 salario DECIMAL (10,2) NULL,
 departamento VARCHAR (100) NOT NULL,
 idade VARCHAR (20) NOT NULL
@@ -36,7 +35,7 @@ WHERE departamento LIKE '%TI%';
 
 SELECT nome,
 salario FROM empregado
-WHERE salario > '55000'; 
+WHERE salario > 55000; 
 
 SELECT * FROM empregado
 ORDER BY idade desc;
@@ -59,34 +58,85 @@ SELECT sum(salario) FROM empregado;
 SELECT sum(salario) FROM empregado
 WHERE departamento = 'vendas';
 
-CREATE TABLE IF NOT EXISTS departamento (
-	id INT AUTO_INCREMENT,
-	nome VARCHAR(30) NOT NULL,
-    
-    PRIMARY KEY(id)
+CREATE TABLE IF NOT exists departamento(
+id int auto_increment,
+nome varchar(30) not null,
+primary key (id)
 );
 
-INSERT INTO departamento (nome) VALUES
-('RH'), 
-('TI'), 
-('Vendas'),
-('Secretaria');
+insert into departamento(nome)
+values
+('rh'),
+('ti'),
+('vendas');
 
-CREATE TABLE IF  NOT EXISTS EMPREGADO2(
-id INT PRIMARY KEY AUTO_INCREMENT ,
-nome VARCHAR (100) NOT NULL,
-cargo VARCHAR (50) NOT NULL,
-salario DECIMAL (10,2) NULL,
-id_departamento int not null,
-idade VARCHAR (20) NOT NULL,
-foreign key (id_departamento) references departamento(id)
+CREATE TABLE IF NOT EXISTS empregado2 (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR (100) NOT NULL,
+	salario DECIMAL (10,2) NULL,
+	idade int NOT NULL,
+    id_departamento int not null,
+    foreign key (id_departamento)
+	references departamento (id)
 );
 
-INSERT INTO EMPREGADO2 (nome, cargo, salario, id_departamento, idade)
-VALUES 
-('João', 'Gerente', 50000, 1, '30'),
-('ana', 'Desenvolvedora', 60000, 2, '28'),
-('Miguel', 'Vendas', 55000, 3, '35');
+select * from empregado2;
 
-select*from empregado2
+INSERT INTO empregado2(
+nome, idade, id_departamento, salario
+)VALUES(
+'joão',
+'30',
+1,
+50000),
+
+('sarah',
+'28',
+2,
+60000),
+
+('Miguel',
+'35',
+3,
+55000),
+
+('ana',
+'27',
+2,
+62000
+);
+
+
+SELECT *FROM empregado2
+INNER JOIN departamento ON empregado2.id_departamento = departamento.id;
+
+select * from empregado2
+left join departamento on empregado2.id_departamento = departamento.id;
+
+
+/*
+exercicio 5
+*/
+SELECT*FROM empregado2 WHERE salario > (SELECT AVG(salario) FROM empregado2
+);
+
+
+/*
+exercicio 6
+*/
+
+insert into empregado2 (nome,idade,id_departamento,salario) values('tomas', 45 ,3, 58.000);
+
+select * from empregado2;
+
+UPDATE empregado2
+SET salario = salario + (salario * 0.05)
+WHERE id_departamento = '3';
+
+delete from empregado2 
+where idade > 40;
+
+
+
+
 
