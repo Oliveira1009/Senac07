75f80a5309b60d5731165cd398d117720ce8b10f
TAMIRES OLIVEIRA <tamires.oliveira6@senacsp.edu.br>
2025-04-14 20:22:50 -0300

    BANCO DE DADOS 7

---
+++ LISTAATIVIDADE/AtividadeRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using LISTAATIVIDADE.BANCODEDADOS;
+using MySql.Data.MySqlClient;
+
+namespace LISTAATIVIDADE
+{
+    internal class AtividadeRepositorio
+    {
+        public void Criar(string titulo)
+        {
+            using (var con = Database.GetConnectin())
+            {
+                con.Open();
+
+                string query = "INSERT INTO atividade(titulo) VALUES (@titulo);";
+
+                using (var cmd = new MySqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@titulo", titulo);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+        }
+        public void AtualizarSituacao(int id, int novaSituacao)
+        {
+            using (var con = Database.GetConnectin())
+            {
+
+                con.Open();
+
+               
+                string query = "UPDATE ATIVIDADE SET situacao =@situacao WHERE id = @id;";
+
+                //E aqui?
+                using (var cmd = new MySqlCommand())
+                {
+                    cmd.Parameters.AddWithValue("@situacao", novaSituacao);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ LISTAATIVIDADE/BANCODEDADOS/Database.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace LISTAATIVIDADE.BANCODEDADOS
+{
+    internal class Database
+    {
+        private static readonly string ConnectionString = "datasourse=localhost;username=root;passaword=;database=secac;";
+
+        public static MySqlConnection GetConnectin()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
+
